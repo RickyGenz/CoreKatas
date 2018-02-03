964b0b6a1f895698ddcdae8d72ff99c7bcc3b520
Ricky Genz <rgenz1@gmail.com>
2018-02-03 11:35:26 -0800

    Add Game

---
+++ BowlingKataTests/GameTest.cs
@@ -0,0 +1,15 @@
+﻿using BowlingKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingKataTests
+{
+    [TestClass]
+    class GameTest
+    {
+        [TestInitialize]
+        void InitializeGame()
+        {
+            Game Game = new Game();
+        }
+    }
+}
