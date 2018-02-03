3c25b50588b21b01858ec20fbc29035bdcd33fb1
Ricky Genz <rgenz1@gmail.com>
2018-02-03 15:10:02 -0800

    Add Game.Roll and Game.Score

--- BowlingKataTests/GameTest.cs
+++ BowlingKataTests/GameTest.cs
@@ -4,12 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BowlingKataTests
 {
     [TestClass]
-    class GameTest
+    public class GameTest
     {
+        Game Game;
+
         [TestInitialize]
-        void InitializeGame()
+        public void InitializeGame()
         {
-            Game Game = new Game();
+            Game = new Game();
+        }
+
+        [TestMethod]
+        public void GutterGame()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Game.Roll(0);
+            }
+            Assert.AreEqual(0, Game.Score);
         }
     }
 }
